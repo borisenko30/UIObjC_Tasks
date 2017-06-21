6e6cc0276486536e3e722e04c199300c8c083767
Student003 <student003@Antons-iMac.local>
2017-06-21 17:15:44 +0300

    Added activityIndicator

---
+++ UITasks/Sources/UI/IDPUsersViewController/Views/IDPPreLaunchUsersView.m
@@ -0,0 +1,21 @@
+//
+//  IDPPreLaunchUsersView.m
+//  UITask01
+//
+//  Created by Student003 on 6/21/17.
+//  Copyright © 2017 Student003. All rights reserved.
+//
+
+#import "IDPPreLaunchUsersView.h"
+
+@implementation IDPPreLaunchUsersView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
