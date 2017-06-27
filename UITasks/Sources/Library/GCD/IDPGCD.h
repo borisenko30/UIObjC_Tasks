c4919b216a570e3a00a5f987bd3403526a13abf7
Student003 <student003@Antons-iMac.local>
2017-06-27 17:29:55 +0300

    Fixed file structure

---
+++ UITasks/Sources/Library/GCD/IDPGCD.h
@@ -0,0 +1,33 @@
+//
+//  IDPDispathchQueue.h
+//  Task09_ObjC
+//
+//  Created by Student003 on 6/8/17.
+//  Copyright © 2017 Student003. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^IDPBlock)();
+typedef BOOL(^IDPRunBlock)();
+
+extern void IDPDispatchOnMainQueue(IDPBlock block);
+
+// dipatch async queue on time interval
+extern void IDPDispatchAsyncInBackgroundWithInterval(NSTimeInterval interval,
+                                                     IDPBlock block,
+                                                     IDPRunBlock running);
+
+// dispatch asynchronous queues
+extern void IDPDispatchAsyncInBackground(IDPBlock block);
+extern void IDPDispatchAsyncWithUtilityPriority(IDPBlock block);
+extern void IDPDispatchAsyncWithInitiatedPriority(IDPBlock block);
+extern void IDPDispatchAsyncWithInteractivePriority(IDPBlock block);
+extern void IDPDispatchAsyncWithDefaultPriority(IDPBlock block);
+
+// dispatch synchronous queues
+extern void IDPDispatchSyncInBackground(IDPBlock block);
+extern void IDPDispatchSyncWithUtilityPriority(IDPBlock block);
+extern void IDPDispatchSyncWithInitiatedPriority(IDPBlock block);
+extern void IDPDispatchSyncWithInteractivePriority(IDPBlock block);
+extern void IDPDispatchSyncWithDefaultPriority(IDPBlock block);
